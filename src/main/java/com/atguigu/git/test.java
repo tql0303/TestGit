0812505f6f55f56ac0c78a37df98e38fe9feb5c1
tom_glo <tom@atguigu.com>
2019-08-10 17:58:09 +0800

    第二次修改，添加了Demo类

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -3,6 +3,6 @@ package com.atguigu.git;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("测试Git");
-		System.out.println("修改");
+		System.out.println("第一次修改");
 	}
 }
