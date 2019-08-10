b2a1935bff68d3057ba3901066abd58ba921c9f8
tom_glo <tom@atguigu.com>
2019-08-10 20:47:55 +0800

    修改为hot_fix分支

--- src/main/java/com/atguigu/git/Demo.java
+++ src/main/java/com/atguigu/git/Demo.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Demo test...");
+		System.out.println("hot_fix Demo test...");
+		System.out.println("left ...");
 	}
 }
