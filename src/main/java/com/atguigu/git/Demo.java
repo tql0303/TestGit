fe1668512bc522ec2e4e612967e3988f9f181504
tom_glo <tom@atguigu.com>
2019-08-11 11:29:46 +0800

    IDEA修改从eclipse克隆的项目

--- src/main/java/com/atguigu/git/Demo.java
+++ src/main/java/com/atguigu/git/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hot_fix Demo test...");
 		System.out.println("left ...");
+		System.out.println("IDEA克隆项目，修改并提交");
 	}
 }
